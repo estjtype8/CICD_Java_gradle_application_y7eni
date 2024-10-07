65bbc5c6c2efd8915e0bd77feb62a5079216d67e
gizem tekiner kuvandık <92186218+estjtype8@users.noreply.github.com>
2024-10-07 16:25:46 +0300

    Update deneme.java

--- deneme.java
+++ deneme.java
@@ -4,5 +4,9 @@ class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello, GİZEM");
+        System.out.println("Hello, TEKİNER");
+        System.out.println("Hello, KUVANDIK");
+        System.out.println("********************************************************");
     }
 }
+//added 3 lines except this at 7.10.2024
